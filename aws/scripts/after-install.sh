54c4291a34429004d6e06cc1571dd387ed8f6a2c
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-16 05:58:29 -0300

    Inclusão de melhorias para deploy e inclusão da cronjob para execução dos playbooks no inventário dinamico com base no ambiente

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,3 +2,13 @@
 set -xe
 
 chown -R ec2-user:ec2-user /etc/ansible
+
+if [ -f /etc/crontab ]; then
+    rm -f /etc/crontab
+fi
+
+echo "Managed by Code Deploy" >> /etc/crontab
+echo "SHELL=/bin/bash" >> /etc/crontab
+echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin" >> /etc/crontab
+echo "MAILTO=root" >> /etc/crontab
+echo "*/10 * * * * ec2-user ansible-playbook -i /etc/ansible/inventory_${ENV}_aws_ec2.yaml /etc/ansible/playbooks/*" >> /etc/crontab
