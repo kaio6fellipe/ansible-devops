f40fb177d4cbe2b021d6f6426804058eba0bbbdc
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-17 03:52:39 -0300

    Inclusão de usuário root nas máquinas

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -11,4 +11,4 @@ echo "Managed by Code Deploy" >> /etc/crontab
 echo "SHELL=/bin/bash" >> /etc/crontab
 echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin" >> /etc/crontab
 echo "MAILTO=root" >> /etc/crontab
-echo "*/10 * * * * ec2-user ansible-playbook -i /etc/ansible/inventory_${ENV}_aws_ec2.yaml /etc/ansible/playbooks/*" >> /etc/crontab
+echo "*/5 * * * * ec2-user ansible-playbook -i /etc/ansible/inventory_${ENV}_aws_ec2.yaml /etc/ansible/playbooks/*" >> /etc/crontab
