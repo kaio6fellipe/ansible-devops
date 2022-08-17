c624d18e05e458a274be3ae3fd2624d4605e5910
kaio6fellipe <kaiofellipe_@hotmail.com>
2022-08-17 04:36:45 -0300

    Inclusão de chave publica no playbook e mudança no processo pós instalação

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -11,4 +11,5 @@ echo "Managed by Code Deploy" >> /etc/crontab
 echo "SHELL=/bin/bash" >> /etc/crontab
 echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin" >> /etc/crontab
 echo "MAILTO=root" >> /etc/crontab
-echo "*/5 * * * * ec2-user ansible-playbook -i /etc/ansible/inventory_${ENV}_aws_ec2.yaml /etc/ansible/playbooks/*" >> /etc/crontab
+ENV_DEPLOY=$(sudo -Hiu root env | grep ENV | cut -c5-7)
+echo "*/5 * * * * ec2-user ansible-playbook -i /etc/ansible/inventory_${ENV_DEPLOY}_aws_ec2.yaml /etc/ansible/playbooks/*" >> /etc/crontab
