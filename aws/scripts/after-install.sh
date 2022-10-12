3803295c697e8cbd6e251bb960db4c2416645903
kaio-fellipe <kaio6fellipe@gmail.com>
2022-10-12 02:01:50 -0300

    Ansible vault updates

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -14,4 +14,4 @@ echo "SHELL=/bin/bash" >> /etc/crontab
 echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin" >> /etc/crontab
 echo "MAILTO=root" >> /etc/crontab
 ENV_DEPLOY=$(sudo -Hiu root env | grep ENV | cut -c5-7)
-echo "*/5 * * * * ec2-user find /etc/ansible/playbooks/ -type f -name \"*.yaml\" -execdir ansible-playbook -i /etc/ansible/inventory_${ENV_DEPLOY}_aws_ec2.yaml {} --vault-password-file /home/ec2-user/.ssh/terraform-aws-${ENV_DEPLOY} \;" >> /etc/crontab
+echo "*/5 * * * * ec2-user find /etc/ansible/playbooks/ -type f -name \"*.yaml\" -execdir ansible-playbook -i /etc/ansible/inventory_${ENV_DEPLOY}_aws_ec2.yaml {} --vault-password-file /home/ec2-user/.vault_password_file-${ENV_DEPLOY} \;" >> /etc/crontab
